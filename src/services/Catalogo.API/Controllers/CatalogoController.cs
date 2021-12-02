a11cd6834ed59963e2b524883cd58afa3796553c
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-12-02 06:46:42 -0300

    Implementações nos serviços

---
+++ src/services/Catalogo.API/Controllers/CatalogoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Catalogo.API.Controllers
+{
+    [ApiController]
+    [Route("api/catalogo")]
+    public class CatalogoController
+    {
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Produto 1", "Produto 2" };
+        }
+    }
+}
---
+++ src/services/Pedido.API/Controllers/PedidoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Pedido.API.Controllers
+{
+    [ApiController]
+    [Route("api/pedido")]
+    public class PedidoController
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Pedido 1", "Pedido 2" };
+        }
+    }
+}

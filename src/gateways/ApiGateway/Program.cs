5012379b3046fa153368c276193c63ef620c6f13
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-12-02 06:59:42 -0300

    Criando a API Gateway

--- src/gateways/ApiGateway/Program.cs
+++ src/gateways/ApiGateway/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace ApiGateway
@@ -7,14 +9,12 @@ namespace ApiGateway
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+                WebHost.CreateDefaultBuilder(args)
+                    .ConfigureAppConfiguration(ic => ic.AddJsonFile("configuration.json"))
+                    .UseStartup<Startup>();
     }
 }
--- src/gateways/ApiGateway/Startup.cs
+++ src/gateways/ApiGateway/Startup.cs
@@ -3,22 +3,23 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace ApiGateway
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
+        private IConfiguration _configuration { get; }
 
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
-
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddOcelot(_configuration);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -28,16 +29,10 @@ namespace ApiGateway
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            // É importante que esta sempre seja a última linha do método Configure.
+            // Pois o middleware do Ocelot é terminal. Ou seja, ele não invoca nenhum outro middleware do pipeline.
+            // Caso a solicitação realizada não seja encontrada nas configurações do Ocelot, ele gera um erro 404.
+            app.UseOcelot().Wait();
         }
     }
 }
